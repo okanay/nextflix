138ef8eaf58eb81b2a9b7b73d01b955c4c89afef
okanay <okanay@hotmail.com>
2023-03-14 18:12:19 +0300

    questions-6

--- components/UI/Index-UI/DivededFull.js
+++ components/UI/Index-UI/DivededFull.js
@@ -1,4 +1,4 @@
 export const DivededFull = () => {
 
-    return <div className={'w-full h-2 bg-skin-theme-body-1000/95'}></div>
+    return <div className={'w-full h-2 bg-skin-theme-body-1000/95 border border-skin-theme-body-1000/0'}></div>
 }
