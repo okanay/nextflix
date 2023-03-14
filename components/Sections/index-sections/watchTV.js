cac0b545d449e1ffe06d5895f72466bc3cdcb420
okanay <okanay@hotmail.com>
2023-03-14 14:59:20 +0300

    offlineWatch-5

--- components/Sections/index-sections/watchTV.js
+++ components/Sections/index-sections/watchTV.js
@@ -12,20 +12,23 @@ export const WatchTV = () => {
                 <h1 className={'text-3xl xlPhone:text-5xl tablet:text-6xl max-w-2xl font-semibold'}>{language.t1}</h1>
                 <h3 className={'text-sm xlPhone:text-2xl tablet:text-xl font-semibold max-w-2xl mt-4'}>{language.t2}</h3>
             </div>
-            <div className={''}>
-                <div className={'relative flex flex-row justify-center'}>
-                    <video autoPlay loop muted className={"w-96 h-96 absolute"}>
-                        <source
-                            src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"
-                            type="video/mp4"/>
+            <div className={'relative flex flex-row justify-center'}>
+                <video autoPlay loop muted className={`
+                        w-[17rem] h-[17rem] top-0
+                        basePhone:w-[19rem] basePhone:h-[19rem] 
+                        lgPhone:w-[21.5rem] lgPhone:h-[21.5rem]
+                        xlPhone:w-[24rem] xlPhone:h-[24rem] top-0
+                        absolute`}>
+                    <source
+                        src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"
+                        type="video/mp4"/>
 
-                    </video>
+                </video>
 
-                    <div className={'relative z-20'}>
-                        <Image src={"/tv.png"} alt={"tv"} width={540} height={540} priority={true} className={''}/>
-                    </div>
-
+                <div className={'relative z-20'}>
+                    <Image src={"/tv.png"} alt={"tv"} width={540} height={540} priority={true} className={''}/>
                 </div>
+
             </div>
         </div>
     </section>)
