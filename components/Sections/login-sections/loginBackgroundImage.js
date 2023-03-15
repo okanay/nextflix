08e32777a6e7fdcf83023efb26f63e6bfeb0e3f4
okanay <okanay@hotmail.com>
2023-03-15 15:33:51 +0300

    login-page-2

--- components/Sections/login-sections/loginBackgroundImage.js
+++ components/Sections/login-sections/loginBackgroundImage.js
@@ -7,25 +7,20 @@ export const LoginBackgroundImage = ({children}) => {
 
 
     return (
-        <div style={{position: 'relative', overflow: 'hidden'}} className={'bg-skin-theme-body-100 h-[100vh]'}>
+        <div style={{position: 'relative'}} className={'w-full h-full bg-black'}>
             <div className={`absolute w-full h-full z-10`}>
                 <div
-                    className={`${loadingImage ? LoginBackgroundImageClass.complete : LoginBackgroundImageClass.loading} absolute z-10 w-full h-full`}
+                    className={`${loadingImage ? LoginBackgroundImageClass.complete : LoginBackgroundImageClass.loading} absolute z-10 w-full h-[100vh] h-full bg-cover`}
                 />
-                <div style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    margin: 'auto',
-                    backgroundImage: 'url(/contents-image.jpg)',
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'left top',
-                    minWidth: '100%',
-                    minHeight: '100%'
-                }}/>
+                <Image
+                    src={'/contents-image.jpg'}
+                    alt={"contents"}
+                    width={2460}
+                    height={1640}
+                    className={'m-auto w-full h-full hidden tablet:block'}
+                    onLoadingComplete={() => {setLoadingImage(true)}}/>
             </div>
+
             <div className={'relative z-40 w-full h-full'}>{children}</div>
         </div>
     );
