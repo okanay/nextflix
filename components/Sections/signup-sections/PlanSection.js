448ae20f81d2ba08b0641b9dc01482da2b3eb1c3
okanay <okanay@hotmail.com>
2023-03-18 16:24:05 +0300

    optimization-18-march-5

--- components/Sections/signup-sections/PlanSection.js
+++ components/Sections/signup-sections/PlanSection.js
@@ -9,7 +9,7 @@ export const PlanSection = () => {
     const language = useSelector(state => state.language.value.signup.plan)
 
 
-    return (<section id={'plan-section'} className={'max-w-[340px] w-full mx-auto h-full mt-20 sm:mt-44'}>
+    return (<section id={'plan-section'} className={'max-w-[340px] w-full mx-auto h-full mb-32 tablet:mb-10 mt-20 sm:mt-44'}>
         <div className={'w-full h-full flex flex-col flex-wrap justify-start items-center scale-95 xlPhone:scale-100'}>
             <Image src={checkmark} alt={"checkmark"} className={'w-12'} priority/>
             <h4 className={'text-xs mt-8'}>{language.p.p1} <b>2</b> {language.p.p2} <b>3</b></h4>
@@ -17,7 +17,7 @@ export const PlanSection = () => {
             <div className={'max-w-[300px]'}>
                 <PlanRules language={language}/>
             </div>
-            <Link href={'/signup/planchoose'} className={'w-full text-center mt-6 py-4 bg-skin-theme-600 rounded max-w-screen-lg w-full text-skin-theme-font-900 text-2xl mb-32 tablet:mb-0'}>{language.b1}</Link>
+            <Link href={'/signup/planchoose'} className={'w-full text-center mt-6 py-4 bg-skin-theme-600 rounded max-w-screen-lg w-full text-skin-theme-font-900 text-2xl'}>{language.b1}</Link>
         </div>
     </section>)
 }
