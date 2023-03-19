fddbe4f7f313a32c8c6ddb904dd5dfd30b2cb74e
okanay <okanay@hotmail.com>
2023-03-19 15:02:05 +0300

    optimization-19-march-9

--- components/UI/Index-UI/watchTVImage.js
+++ components/UI/Index-UI/watchTVImage.js
@@ -4,19 +4,19 @@ export const WatchTVImage = ({language}) => {
 
     return (<div className={'relative flex flex-row justify-center content-start'}>
             <div className={`
-                        w-[20rem] h-[20rem]
-                        basePhone:w-[24.5rem] basePhone:h-[22rem] 
-                        xlPhone:w-[26rem] xlPhone:h-[24rem] 
-                        sm:w-[40rem] sm:h-[34rem] 
-                        tablet:w-[44rem] tablet:h-[36rem] 
+                        w-[20rem] h-[16rem]
+                        basePhone:w-[24.5rem] basePhone:h-[18rem] 
+                        xlPhone:w-[26rem] xlPhone:h-[20rem] 
+                        sm:w-[40rem] sm:h-[31rem] 
+                        tablet:w-[44rem] tablet:h-[31rem] 
                         relative flex flex-row justify-center`}>
                 <Image src={watchImage} alt={"tv"} className={'z-20'} priority/>
                 <video autoPlay loop muted playsInline className={`
-                        w-[15rem] h-[22rem] -top-9
+                        w-[15rem] h-[22rem] -top-14
                         basePhone:w-[18rem] basePhone:h-[17.5rem] basePhone:top-0
-                        xlPhone:w-[19.5rem] xlPhone:h-[22rem] xlPhone:top-0
-                        sm:w-[29rem] sm:h-[28rem] sm:top-10
-                        tablet:w-[29rem] tablet:h-[36rem] tablet:-top-9
+                        xlPhone:w-[19.5rem] xlPhone:h-[22rem] xlPhone:-top-6
+                        sm:w-[29rem] sm:h-[36rem] sm:-top-12
+                        tablet:w-[29rem] tablet:h-[36rem] tablet:-top-12
                         absolute z-0`}>
                     <source
                         src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"
