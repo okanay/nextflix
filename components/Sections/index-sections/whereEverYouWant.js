7f577dda68f7c024def7ef699564717669beaf42
okanay <okanay@hotmail.com>
2023-05-02 20:50:19 +0300

    fix

--- components/Sections/index-sections/whereEverYouWant.js
+++ components/Sections/index-sections/whereEverYouWant.js
@@ -1,7 +1,7 @@
 import {WhereEverYouWantImage} from "../../UI/Index-UI/whereEverYouWantImage";
 
 export const WhereEverYouWant = ({language}) => {
-    return (<section id={'where-ever-you-want-watch'} className={'bg-skin-theme-body-1000 py-16 tablet:py-0 tablet:pt-16'}>
+    return (<section id={'where-ever-you-want-watch'} className={'bg-skin-theme-body-1000 pt-16 tablet:pb-8'}>
         <div className={'flex flex-row flex-wrap justify-center items-center gap-4 xlPhone:gap-8'}>
             <div className={'text-skin-theme-font-900 px-8 tablet:px-4 text-center xlPhone:text-start'}>
                 <h1 className={'text-3xl lgPhone:text-4xl tablet:text-5xl max-w-2xl font-semibold'}>{language.t1}</h1>
